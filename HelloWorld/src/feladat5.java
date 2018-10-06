a0f80dde6409b36b99b1e52e60c42cda9c5b79b3
Balázs Kis <kis.balazs2014@outlook.com>
2018-10-06 13:04:12 +0200

    feladat5

---
+++ HelloWorld/src/feladat5.java
@@ -0,0 +1,12 @@
+
+public class feladat5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+/*Írj programot, amely a paraméterként adott két számról eldönti, hogy
+melyik a nagyobb! Vizsgáld meg, hogy a felhasználó valóban két
+paramétert adott-e meg; két érték esetén hajtsd végre az összehasonlítást,
+ellenkezo esetben írj ki hibaüzenetet! */
+	}
+
+}
