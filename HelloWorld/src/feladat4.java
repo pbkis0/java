fbbdbc92653846ec991976753ae90a199956d6bc
Balázs Kis <kis.balazs2014@outlook.com>
2018-10-06 13:00:19 +0200

    feladat4 - megoldása

--- HelloWorld/src/feladat4.java
+++ HelloWorld/src/feladat4.java
@@ -3,7 +3,18 @@ public class feladat4 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+/* Írj programot, mely egy 1 és 1 000 000 közötti számról eldönti, hogy hány
+jegyu! */
+		char[] teszt = args[0].toCharArray();
+		if(teszt[0] == '-') {
+		System.out.println("Ennyi számjegyű: " + (teszt.length-1)); //Ha negatív (-) számot aduk meg, akkor vegyen el belőle egyet, és úgy fusson le a program.
+		} else {
+			System.out.println("Ennyi számjegyű: " + teszt.length); //Ha pozitiv számot adunk meg, úgy fusson le a program.
+		}
+		
+		
+		
+		
 	}
 
 }
