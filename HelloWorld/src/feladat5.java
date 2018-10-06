77403d573b20aaef9e0412355d3c34fb2b660bde
Balázs Kis <kis.balazs2014@outlook.com>
2018-10-06 13:27:14 +0200

    feladat5

--- HelloWorld/src/feladat5.java
+++ HelloWorld/src/feladat5.java
@@ -7,6 +7,17 @@ public class feladat5 {
 melyik a nagyobb! Vizsgáld meg, hogy a felhasználó valóban két
 paramétert adott-e meg; két érték esetén hajtsd végre az összehasonlítást,
 ellenkezo esetben írj ki hibaüzenetet! */
+		
+		int a = Integer.parseInt(args[0]);
+		int b = Integer.parseInt(args[1]);
+		
+		
+		if (a > b) {
+			System.out.println("Az első szám nagyobb.");
+		} else {
+			System.out.println("A második szám a nagyobb.");
+		}
+		
 	}
 
 }
